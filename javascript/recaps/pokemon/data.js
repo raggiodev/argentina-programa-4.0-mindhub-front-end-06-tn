7dbf5b30a1286486be083ed24a020de6438bee5b
Fernando Andrés Raggio <fernandoandresraggio@gmail.com>
2023-03-07 22:49:40 -0300

    Create data.js

---
+++ javascript/recaps/pokemon/data.js
@@ -0,0 +1,157 @@
+const data = [
+    {
+        "id": 1,
+        "name": "Bulbasaur",
+        "type": ["Grass", "Poison"],
+        "hp": 45,
+        "attack": 49,
+        "defense": 49
+    },
+    {
+        "id": 2,
+        "name": "Charmander",
+        "type": ["Fire"],
+        "hp": 39,
+        "attack": 52,
+        "defense": 43
+    },
+    {
+        "id": 3,
+        "name": "Squirtle",
+        "type": ["Water"],
+        "hp": 44,
+        "attack": 48,
+        "defense": 65
+    },
+    {
+        "id": 4,
+        "name": "Pikachu",
+        "type": ["Electric"],
+        "hp": 35,
+        "attack": 55,
+        "defense": 40
+    },
+    {
+        "id": 5,
+        "name": "Jigglypuff",
+        "type": ["Normal", "Fairy"],
+        "hp": 115,
+        "attack": 45,
+        "defense": 20
+    },
+    {
+        "id": 6,
+        "name": "Geodude",
+        "type": ["Rock", "Ground"],
+        "hp": 40,
+        "attack": 80,
+        "defense": 100
+    },
+    {
+        "id": 7,
+        "name": "Abra",
+        "type": ["Psychic"],
+        "hp": 25,
+        "attack": 20,
+        "defense": 15
+    },
+    {
+        "id": 8,
+        "name": "Alakazam",
+        "type": ["Psychic"],
+        "hp": 55,
+        "attack": 50,
+        "defense": 45
+    },
+    {
+        "id": 9,
+        "name": "Machop",
+        "type": ["Fighting"],
+        "hp": 70,
+        "attack": 80,
+        "defense": 50
+    },
+    {
+        "id": 10,
+        "name": "Gengar",
+        "type": ["Ghost", "Poison"],
+        "hp": 60,
+        "attack": 65,
+        "defense": 60
+    },
+    {
+        "id": 11,
+        "name": "Mr. Mime",
+        "type": ["Psychic", "Fairy"],
+        "hp": 40,
+        "attack": 45,
+        "defense": 65
+    },
+    {
+        "id": 12,
+        "name": "Koffing",
+        "type": ["Poison"],
+        "hp": 40,
+        "attack": 65,
+        "defense": 95
+    },
+    {
+        "id": 13,
+        "name": "Diglett",
+        "type": ["Ground"],
+        "hp": 10,
+        "attack": 55,
+        "defense": 25
+    },
+    {
+        "id": 14,
+        "name": "Dugtrio",
+        "type": ["Ground"],
+        "hp": 35,
+        "attack": 100,
+        "defense": 50
+    },
+    {
+        "id": 15,
+        "name": "Pidgey",
+        "type": ["Normal", "Flying"],
+        "hp": 40,
+        "attack": 45,
+        "defense": 40
+    },
+    {
+        "id": 16,
+        "name": "Rattata",
+        "type": ["Normal"],
+        "hp": 30,
+        "attack": 56,
+        "defense": 35
+    },
+    {
+        "id": 17,
+        "name": "Spearow",
+        "type": ["Normal", "Flying"],
+        "hp": 40,
+        "attack": 60,
+        "defense": 30
+    },
+    {
+        "id": 18,
+        "name": "Ekans",
+        "type": ["Poison"],
+        "hp": 35,
+        "attack": 60,
+        "defense": 44
+    },
+
+    {
+        "id": 19,
+        "name": "Sandshrew",
+        "type": ["Ground"],
+        "hp": 50,
+        "attack": 75,
+        "defense": 85
+    }
+]
+
+export default data
